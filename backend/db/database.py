1bdc92ceb132afacade11d19d6185b78b2def48e
5ud03r5 <kmarek973@gmail.com>
2023-04-19 09:06:36 +0200

    fix: modified fastapi to be more flexible while setting up dev env

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,8 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
+
+DB = None
+
+if 'DATABASE_URL' not in os.environ:
+    DB = "postgresql://postgres:postgrespw@localhost:32768"
+else:
+    DB = os.environ['DATABASE_URL']
+
+SQLALCHEMY_DATABASE_URL = DB
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
